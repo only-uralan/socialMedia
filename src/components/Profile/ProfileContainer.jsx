b1f419ab0277f2ebe90cc2d77d40f764489afa57
a.lidzhiev2 <arsik0990@gmail.com>
2022-12-13 14:36:10 +0600

    add useParams and func withRouter

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,16 @@ import Profile from "./Profile";
 import axios from "axios";
 import { connect } from "react-redux";
 import { setUserProfile } from "../../redux/profileReducer";
+import { useParams } from "react-router-dom";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.router.params.userId;
+    if (!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
@@ -22,4 +27,15 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let params = useParams();
+    return <Component {...props} router={{ params }} />;
+  }
+
+  return ComponentWithRouterProp;
+}
+
+export default connect(mapStateToProps, { setUserProfile })(
+  withRouter(ProfileContainer)
+);
